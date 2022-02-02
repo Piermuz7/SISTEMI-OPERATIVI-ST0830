df7eaa4f6980b2ef05f2834f4f89c0fba7ec74ff
Piermichele Rosati <piermichele.rosati@gmail.com>
2022-02-02 21:55:50 +0100

    Update bvernan.h

--- bvernan/PiermicheleRosati109213/bvernan.h
+++ bvernan/PiermicheleRosati109213/bvernan.h
@@ -4,7 +4,7 @@
 /**
  * Funzione che presi in input i path assoluti o relativi del keyfile,
  * inputfile e outputfile, cripta l' inputfile applicando il cifrario
- * bvernan con il keyfile e scrivendo il risultato ottenuto nell' utputfile.
+ * bvernan con il keyfile e scrivendo il risultato ottenuto nell' outputfile.
  * @param keyfile la sequenza di byte rappresentante la chiave.
  * @param inputfile la sequenza di byte da criptare.
  * @param outputfile la sequenza di byte criptati.
